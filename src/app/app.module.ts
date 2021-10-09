750017e72b76334d3ea147f6d4fac7342a3414a7
Glaison <glaisonc@gmail.com>
2021-10-09 12:26:26 -0300

    atualização

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 
+import { CurseLitsComponent } from './curses/curse-lits.component';
+import { starComponent } from './star/star.component';
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CurseLitsComponent,
+    starComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/curses/curse-lits.component.ts
+++ src/app/curses/curse-lits.component.ts
@@ -17,7 +17,7 @@ export class CurseLitsComponent implements OnInit {
     this.courses = [
       {id: 1,
         name: 'Angular',
-        imageUrl:'',
+        imageUrl:'/assets/images/forms.png',
         price: 99.99,
         code:'XPS-8796',
         duration:120,
@@ -26,20 +26,20 @@ export class CurseLitsComponent implements OnInit {
       },
       {id: 2,
         name: 'JAVA',
-        imageUrl:'',
+        imageUrl:'/assets/images/http.png',
         price: 399.99,
         code:'JAVA-8796',
         duration:80,
-        rating:4,
+        rating:3,
         releaseDate:'Octuber, 09, 2021'
       },
       {id: 3,
         name: 'Ruby',
-        imageUrl:'',
+        imageUrl:'/assets/images/router.png',
         price: 89,
         code:'RRB-8796',
         duration:80,
-        rating:4,
+        rating:2.5,
         releaseDate:'Octuber, 09, 2020'
       }
 
